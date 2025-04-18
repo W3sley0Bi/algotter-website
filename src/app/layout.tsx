928102f323e57acae40b762d010e18bbeb64f68e
Wesley <obi.wesley.giovanni@gmail.com>
2025-04-18 19:57:40 +0200

    implemented vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Sans } from 'next/font/google'
 import './globals.css'
 import { ThemeSwitcher } from '@/components/theme-switcher'
 import { ThemeProvider } from '@/components/theme-provider'
+import { Analytics } from "@vercel/analytics/react"
 
 const dmSans = DM_Sans({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={dmSans.className}>
+        <Analytics/>
         <ThemeProvider attribute="class" disableTransitionOnChange>
           {children}
           <ThemeSwitcher />
